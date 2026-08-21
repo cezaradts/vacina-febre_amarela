1bbc3944629200b083d87aec8e68a6350ed5c970
cezaradts <cezaradts@gmail.com>
2026-08-20 23:34:20 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { App } from './App'
 import './styles.css'
 
-// Configura o router gerado pelo TanStack
-const router = createRouter({ routeTree })
-
-// Registra o router para segurança de tipos
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-const rootElement = document.getElementById('root')!
-
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>,
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
