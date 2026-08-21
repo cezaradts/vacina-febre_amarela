764c03be1d90a001b7baa4a9b2855815ca8fc1a1
cezaradts <cezaradts@gmail.com>
2026-08-20 23:19:19 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  css: {
-    transformer: "postcss",
-  },
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: "::",
+    host: '0.0.0.0',
     port: 8080,
   },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  base: '/',
-}));
+})
