4c19164256aff43e27a8e3b7bc17c0b6a42be10d
cezaradts <cezaradts@gmail.com>
2026-08-20 22:49:29 -0300

    Replace router with App component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { App } from './App'
 import './styles.css'
 
-const router = createRouter({ routeTree })
-
-const rootElement = document.getElementById('root')
-
-if (rootElement && !rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
